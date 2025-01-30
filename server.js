5250df5d77006de51992141c18bb6bfad8877b59
unknown <brenogomes6162@digitalcollege.internal>
2025-01-30 20:05:28 -0300

    iniciando servidor

---
+++ server.js
@@ -0,0 +1,11 @@
+import express from 'express'
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('Servidor funcionando!')
+})
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+})
