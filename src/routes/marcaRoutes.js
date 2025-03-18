81e1fed782148cd7843bfb546ccbbe19dc0bbb34
breno-oliveira98 <breno.oliveira2011@hotmail.com>
2025-03-18 15:51:37 -0300

    subindo ultima alteração

--- src/routes/marcaRoutes.js
+++ src/routes/marcaRoutes.js
@@ -4,12 +4,13 @@ import MarcaController from "../controllers/marca.controller.js";
 import { validarRequisicao } from '../middlewares/validarRequisicao.js'
 import { marcaSchema } from "../schemas/createMarcaSchema.js";
 import { validarNomeMarca } from "../middlewares/validarNomeDaMarca.js";
+import { authenticateToken } from "../middlewares/authenticateToken.js"
 
 const router = express.Router();
 
 // Definir as rotas
 router.post("/marcas", validarRequisicao(marcaSchema), validarNomeMarca, MarcaController.criar);
-router.get("/marcas", MarcaController.listar);
+router.get("/marcas", authenticateToken, MarcaController.listar);
 router.get("/marcas/:id", MarcaController.buscarPorId);
 router.put("/marcas/:id", MarcaController.atualizar);
 router.delete("/marcas/:id", MarcaController.deletar);
